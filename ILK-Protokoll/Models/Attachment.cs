5a2e55961383d6405aa3662688371ef0bc24bd5c
Julius Heins <hz@iwb.tum.de>
2014-06-05 10:08:32 +0200

    WIP Dateiupload

--- ILK-Protokoll/Models/Attachment.cs
+++ ILK-Protokoll/Models/Attachment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 using System.Linq;
 using System.Web;
 
@@ -24,6 +25,15 @@ namespace ILK_Protokoll.Models
 
 		[Display(Name = "Uploaddatum")]
 		public DateTime Created { get; set; }
+
+		[Display(Name = "Dateigröße")]
+		public uint FileSize { get; set; }
+
+		[NotMapped]
+		public string FileExtension
+		{
+			get { return Path.GetExtension(Name); }
+		}
 
 		[NotMapped]
 		public string URL
