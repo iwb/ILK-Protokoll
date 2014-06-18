8dce4759530541e707882a6a2e3bbc87abbcea91
Julius Heins <hz@iwb.tum.de>
2014-06-18 13:45:31 +0200

    Listen begonnen

---
+++ ILK-Protokoll/Areas/Session/Models/Lists/EmployeePresentation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ILK_Protokoll.Areas.Session.Models.Lists
+{
+	public class EmployeePresentation
+	{
+	}
+}
---
+++ ILK-Protokoll/Areas/Session/Models/Lists/Event.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ILK_Protokoll.Areas.Session.Models.Lists
+{
+	/// <summary>
+	/// Termine und Veranstaltungen, die das ganze Institut betreffen
+	/// </summary>
+	public class Event
+	{
+	}
+}
