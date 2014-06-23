9d501a5046475c3aa47eb56392097852c1aae524
Julius Heins <hz@iwb.tum.de>
2014-06-23 08:20:46 +0200

    Editor für die Anwesenheitsliste gestyled

--- ILK-Protokoll/Areas/Session/Controllers/MasterController.cs
+++ ILK-Protokoll/Areas/Session/Controllers/MasterController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ILK_Protokoll.Areas.Session.Models;
 using ILK_Protokoll.Controllers;
 
 namespace ILK_Protokoll.Areas.Session.Controllers
@@ -12,7 +13,7 @@ namespace ILK_Protokoll.Areas.Session.Controllers
 		// GET: Session/Master
 		public ActionResult Index()
 		{
-			return View(new Models.ActiveSession(db.SessionTypes.First()));
+			return View(new ActiveSession(db.SessionTypes.First()));
 		}
 	}
 }
