ba6244da56f418e053c7aefaa2f077485a386e4c
Julius Heins <hz@iwb.tum.de>
2014-06-27 09:32:28 +0200

    Migrationen aktiviert

--- ILK-Protokoll/Global.asax.cs
+++ ILK-Protokoll/Global.asax.cs
@@ -1,7 +1,10 @@
-﻿using System.Web;
+﻿using System.Data.Entity;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ILK_Protokoll.DataLayer;
+using ILK_Protokoll.Migrations;
 
 namespace ILK_Protokoll
 {
@@ -13,6 +16,8 @@ namespace ILK_Protokoll
 			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
 			RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+			Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataContext, Configuration>());
 
 #if DEBUG
 			foreach (Bundle bundle in BundleTable.Bundles)
