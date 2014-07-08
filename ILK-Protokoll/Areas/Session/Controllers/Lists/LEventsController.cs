6adc47fa815e9f54934c14f0f08b6682c7021d04
Julius Heins <hz@iwb.tum.de>
2014-07-08 15:15:46 +0200

    Methoden in die Elternklasse verschoben, um die anderen Listen vorzubereiten

--- ILK-Protokoll/Areas/Session/Controllers/Lists/LEventsController.cs
+++ ILK-Protokoll/Areas/Session/Controllers/Lists/LEventsController.cs
@@ -1,84 +1,12 @@
-﻿using System.Data.Entity;
-using System.Linq;
-using System.Net;
-using System.Web.Mvc;
-using ILK_Protokoll.Areas.Session.Models.Lists;
+﻿using ILK_Protokoll.Areas.Session.Models.Lists;
 
 namespace ILK_Protokoll.Areas.Session.Controllers.Lists
 {
 	public class LEventsController : ParentController<Event>
 	{
-		// GET: Session/Events
-		public PartialViewResult _List()
+		public LEventsController()
 		{
-			return PartialView(db.LEvents.ToList());
-		}
-
-		// GET: Session/Events/Create
-		public PartialViewResult _CreateForm()
-		{
-			return PartialView("_CreateForm", new Event());
-		}
-
-		// GET: Session/Events/FetchRow
-		public PartialViewResult _FetchRow(int eventID)
-		{
-			return PartialView("_Row", db.LEvents.Find(eventID));
-		}
-
-		// POST: Session/Events/Create
-		// Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
-		// finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult _Create([Bind(Include = "StartDate,EndDate,Time,Place,Description,Organizer")] Event ev)
-		{
-			if (!ModelState.IsValid)
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-			db.LEvents.Add(ev);
-			db.SaveChanges();
-			return PartialView("_Row", ev);
-		}
-
-		// POST: Session/Events/Edit
-		public ActionResult _BeginEdit(int eventID)
-		{
-			var ev = db.LEvents.Find(eventID);
-			if (ev == null)
-				return HttpNotFound();
-
-			return PartialView("_Edit", ev);
-		}
-
-		// POST: Session/Events/Edit
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult _Edit([Bind(Include = "ID,StartDate,EndDate,Time,Place,Description,Organizer")] Event input)
-		{
-			if (!ModelState.IsValid)
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-			db.Entry(input).State = EntityState.Modified;
-			db.SaveChanges();
-			return PartialView("_Row", input);
-		}
-
-		// POST: Session/Events/Delete
-		[HttpPost]
-		public ActionResult _Delete(int? eventID)
-		{
-			if (eventID == null)
-				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-
-			Event ev = db.LEvents.Find(eventID.Value);
-			if (ev == null)
-				return HttpNotFound();
-
-			db.LEvents.Remove(db.LEvents.Find(eventID));
-			db.SaveChanges();
-
-			return new HttpStatusCodeResult(HttpStatusCode.NoContent);
+			_dbSet = db.LEvents;
 		}
 	}
 }
--- ILK-Protokoll/Areas/Session/Controllers/Lists/ParentController.cs
+++ ILK-Protokoll/Areas/Session/Controllers/Lists/ParentController.cs
@@ -1,17 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Net;
 using System.Web.Mvc;
 using ILK_Protokoll.Areas.Session.Models.Lists;
-using ILK_Protokoll.Controllers;
 
 namespace ILK_Protokoll.Areas.Session.Controllers.Lists
 {
+// ReSharper disable Mvc.PartialViewNotResolved
 	public class ParentController<TModel> : SessionBaseController
-		where TModel : BaseItem
+		where TModel : BaseItem, new()
 	{
+		protected DbSet<TModel> _dbSet;
 
+		public PartialViewResult _List()
+		{
+			return PartialView(_dbSet.ToList());
+		}
 
+		public PartialViewResult _CreateForm()
+		{
+			return PartialView("_CreateForm", new TModel());
+		}
+
+		public PartialViewResult _FetchRow(int id)
+		{
+			return PartialView("_Row", _dbSet.Find(id));
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult _Create([Bind(Include = "StartDate,EndDate,Time,Place,Description,Organizer")] TModel ev)
+		{
+			if (!ModelState.IsValid)
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			_dbSet.Add(ev);
+			db.SaveChanges();
+			return PartialView("_Row", ev);
+		}
+
+		public ActionResult _BeginEdit(int id)
+		{
+			var ev = _dbSet.Find(id);
+			if (ev == null)
+				return HttpNotFound();
+
+			return PartialView("_Edit", ev);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult _Edit([Bind(Include = "ID,StartDate,EndDate,Time,Place,Description,Organizer")] TModel input)
+		{
+			if (!ModelState.IsValid)
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			db.Entry(input).State = EntityState.Modified;
+			db.SaveChanges();
+			return PartialView("_Row", input);
+		}
+
+		[HttpPost]
+		public ActionResult _Delete(int? id)
+		{
+			if (id == null)
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			var ev = _dbSet.Find(id.Value);
+			if (ev == null)
+				return HttpNotFound();
+
+			_dbSet.Remove(_dbSet.Find(id));
+			db.SaveChanges();
+
+			return new HttpStatusCodeResult(HttpStatusCode.NoContent);
+		}
 	}
+
+// ReSharper restore Mvc.PartialViewNotResolved
 }
