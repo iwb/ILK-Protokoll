868da0e9af6565c733acbe890dab76496dd04928
Julius Heins <hz@iwb.tum.de>
2014-07-10 15:06:11 +0200

    Vorbereitung für Dateianhänge in den Präsis

--- ILK-Protokoll/Areas/Session/Controllers/Lists/LEmployeePresentationController.cs
+++ ILK-Protokoll/Areas/Session/Controllers/Lists/LEmployeePresentationController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Data.Entity;
+using System.Net;
+using System.Web.Mvc;
 using ILK_Protokoll.Areas.Session.Models.Lists;
 
 namespace ILK_Protokoll.Areas.Session.Controllers.Lists
@@ -20,6 +22,33 @@ namespace ILK_Protokoll.Areas.Session.Controllers.Lists
 		{
 			ViewBag.UserList = new SelectList(db.GetUserOrdered(GetCurrentUser()), "ID", "ShortName");
 			return base._BeginEdit(id);
+		}
+
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+			EmployeePresentation presentation = _dbSet.Find(id);
+			if (presentation == null)
+				return HttpNotFound();
+
+			ViewBag.UserList = new SelectList(db.GetUserOrdered(GetCurrentUser()), "ID", "ShortName");
+			return View(presentation);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public virtual ActionResult Edit([Bind(Exclude = "Created")] EmployeePresentation input)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(input).State = EntityState.Modified;
+				db.SaveChanges();
+				return RedirectToAction("Index", "Lists", new {Area = "Session"});
+			}
+			ViewBag.UserList = new SelectList(db.GetUserOrdered(GetCurrentUser()), "ID", "ShortName");
+			return View(input);
 		}
 	}
 }
