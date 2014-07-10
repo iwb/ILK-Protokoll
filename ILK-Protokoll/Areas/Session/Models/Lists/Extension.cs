2d9fd3ca0cf5b7475de0c207fba3a054c2fb25e9
Julius Heins <hz@iwb.tum.de>
2014-07-10 13:35:49 +0200

    Liste Vertragsverlängerungen implementiert

--- ILK-Protokoll/Areas/Session/Models/Lists/Extension.cs
+++ ILK-Protokoll/Areas/Session/Models/Lists/Extension.cs
@@ -11,13 +11,20 @@ namespace ILK_Protokoll.Areas.Session.Models.Lists
 	[Table("L_Extension")]
 	public class Extension : BaseItem
 	{
+		public Extension()
+		{
+			EndDate = DateTime.Today.AddYears(2);
+		}
+
 		[DisplayName("Mitarbeiter")]
 		public string Employee { get; set; }
 
+		[DisplayName("Vertragsende")]
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 		public DateTime EndDate { get; set; }
 
+		[DisplayName("1/2/3")]
 		[Range(1, 3)]
 		public int ExtensionNumber { get; set; }
 
