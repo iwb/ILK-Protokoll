4339e32c507a62ca1acc88011364557d301110a1
Julius Heins <hz@iwb.tum.de>
2014-07-14 15:55:17 +0200

    Bundle erweitert

--- ILK-Protokoll/App_Start/BundleConfig.cs
+++ ILK-Protokoll/App_Start/BundleConfig.cs
@@ -30,9 +30,19 @@ namespace ILK_Protokoll
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 				"~/Content/iwbTheme.css",
 				"~/Content/themes/base/jquery.ui.core.css",
+				"~/Content/themes/base/jquery.ui.accordion.css",
 				"~/Content/themes/base/jquery.ui.autocomplete.css",
 				"~/Content/themes/base/jquery.ui.button.css",
 				"~/Content/themes/base/jquery.ui.datepicker.css",
+				"~/Content/themes/base/jquery.ui.dialog.css",
+				"~/Content/themes/base/jquery.ui.menu.css",
+				"~/Content/themes/base/jquery.ui.progressbar.css",
+				"~/Content/themes/base/jquery.ui.resizable.css",
+				"~/Content/themes/base/jquery.ui.selectable.css",
+				"~/Content/themes/base/jquery.ui.slider.css",
+				"~/Content/themes/base/jquery.ui.spinner.css",
+				"~/Content/themes/base/jquery.ui.tabs.css",
+				"~/Content/themes/base/jquery.ui.tooltip.css",
 				"~/Content/themes/base/jquery.ui.theme.css"));
 
 			// Festlegen von EnableOptimizations auf false für Debugzwecke. Weitere Informationen
