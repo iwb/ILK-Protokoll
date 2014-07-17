c5ee5448dbb045710701accc81a8167dfb19c3a7
Julius Heins <hz@iwb.tum.de>
2014-07-17 16:10:56 +0200

    Liste "Vakante Stellen" hinzugefügt

---
+++ ILK-Protokoll/Areas/Session/Controllers/Lists/LOpeningsController.cs
@@ -0,0 +1,12 @@
+﻿using ILK_Protokoll.Areas.Session.Models.Lists;
+
+namespace ILK_Protokoll.Areas.Session.Controllers.Lists
+{
+	public class LOpeningsController : ParentController<Opening>
+	{
+		public LOpeningsController()
+		{
+			_dbSet = db.LOpenings;
+		}
+	}
+}
--- ILK-Protokoll/Areas/Session/Models/Lists/opening.cs
+++ ILK-Protokoll/Areas/Session/Models/Lists/opening.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using ILK_Protokoll.util;
 
 namespace ILK_Protokoll.Areas.Session.Models.Lists
 {
@@ -11,17 +12,26 @@ namespace ILK_Protokoll.Areas.Session.Models.Lists
 	[Table("L_Opening")]
 	public class Opening : BaseItem
 	{
+		public Opening()
+		{
+			Start = DateTime.Today;
+		}
+
+		[DisplayName("Projekt")]
 		public string Project { get; set; }
 
 		[DisplayName("Beginn")]
 		[DataType(DataType.Date)]
-		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+		[FutureDate]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
 		public DateTime Start { get; set; }
 
 		public string TG { get; set; }
 
+		[DisplayName("Prof.")]
 		public Prof Prof { get; set; }
 
+		[DisplayName("Beschreibung / Profil")]
 		public string Description { get; set; }
 	}
 }
