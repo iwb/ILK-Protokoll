ec90188d467c6b159ba43a1a4a1b23558c2f685d
Julius Heins <hz@iwb.tum.de>
2014-07-25 11:08:34 +0200

    Web.config Transformation berichtigt

--- ILK-Protokoll/Areas/Session/Controllers/FinalizeController.cs
+++ ILK-Protokoll/Areas/Session/Controllers/FinalizeController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using ILK_Protokoll.Areas.Session.Models;
 using ILK_Protokoll.Models;
@@ -34,6 +37,14 @@ namespace ILK_Protokoll.Areas.Session.Controllers
 		{
 			var session = GetSession();
 			session.End = DateTime.Now;
+			session.LockedTopics = db.TopicLocks
+				.Include(tl => tl.Session)
+				.Include(tl => tl.Topic)
+				.ToList();
+
+			foreach ( var tl in session.LockedTopics)
+				tl.Topic.IsReadOnly = true;
+			
 
 			string html = HelperMethods.RenderViewAsString(ControllerContext, "SessionReport", session);
 			byte[] pdfcontent = HelperMethods.ConvertHTMLToPDF(html);
@@ -50,49 +61,62 @@ namespace ILK_Protokoll.Areas.Session.Controllers
 				.Where(t => t.Lock.Session.ID == session.ID)
 				.ToList();
 
-			foreach (var t in topics)
+			//foreach (var t in topics)
+			//{
+			//	switch (t.Lock.Action)
+			//	{
+			//		case TopicAction.None:
+			//			break;
+			//		case TopicAction.Decide:
+			//			t.IsReadOnly = true;
+			//			t.Decision = new Decision()
+			//			{
+			//				OriginTopic = t,
+			//				Report = report,
+			//				Text = t.Proposal,
+			//				Type = DecisionType.Resolution
+			//			};
+			//			break;
+			//		case TopicAction.Close:
+			//			t.IsReadOnly = true;
+			//			t.Decision = new Decision()
+			//			{
+			//				OriginTopic = t,
+			//				Report = report,
+			//				Text = t.Proposal,
+			//				Type = DecisionType.Closed
+			//			};
+			//			break;
+			//		case TopicAction.Delete:
+			//			db.Votes.RemoveRange(db.Votes.Where(v => v.Topic.ID == t.ID));
+			//			db.Topics.Remove(t);
+			//			break;
+			//		default:
+			//			throw new ArgumentOutOfRangeException();
+			//	}
+			//}
+			try
 			{
-				switch (t.Lock.Action)
-				{
-					case TopicAction.None:
-						break;
-					case TopicAction.Decide:
-						t.IsReadOnly = true;
-						t.Decision = new Decision()
-						{
-							OriginTopic = t,
-							Report = report,
-							Text = t.Proposal,
-							Type = DecisionType.Resolution
-						};
-						break;
-					case TopicAction.Close:
-						t.IsReadOnly = true;
-						t.Decision = new Decision()
-						{
-							OriginTopic = t,
-							Report = report,
-							Text = t.Proposal,
-							Type = DecisionType.Closed
-						};
-						break;
-					case TopicAction.Delete:
-						db.Topics.Remove(t);
-						break;
-					default:
-						throw new ArgumentOutOfRangeException();
-				}
+				db.SaveChanges();
 			}
-			db.SaveChanges();
+			catch (System.Data.DataException ex)
+			{
+				return HTTPStatus(HttpStatusCode.InternalServerError, ex.Message);
+			}
 
 			return PartialView("_ReportSuccess", 3);
 		}
 
 		public ActionResult ShowReport()
 		{
-			var report = SessionReport.FromActiveSession(GetSession());
+			var session = GetSession();
+			session.End = DateTime.Now;
+			session.LockedTopics = db.TopicLocks
+				.Include(tl => tl.Session)
+				.Include(tl => tl.Topic)
+				.ToList();
 
-			return View("SessionReport", report);
+			return View("SessionReport", session);
 		}
 	}
 }
