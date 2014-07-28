3793e37eb46c9714e35eb97b859bf569f077921c
Julius Heins <hz@iwb.tum.de>
2014-07-28 09:37:14 +0200

    Authentifizierung geändert, damit wiederkehrende Aufgaben (E-mail verschicken) auch ohne Authentifizierung erledigt werden können.

--- ILK-Protokoll/App_Start/FilterConfig.cs
+++ ILK-Protokoll/App_Start/FilterConfig.cs
@@ -7,6 +7,8 @@ namespace ILK_Protokoll
 		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
 		{
 			filters.Add(new HandleErrorAttribute());
+			//filters.Add(new RequireHttpsAttribute());
+			filters.Add(new AuthorizeAttribute { Roles = @"IWBMUC\ILK,IWBMUC\hz" });
 		}
 	}
 }
---
+++ ILK-Protokoll/Controllers/ScheduledTasksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ILK_Protokoll.Controllers
+{
+    public class ScheduledTasksController : Controller
+    {
+        // GET: ScheduledTasks
+		 [AllowAnonymous]
+        public string Index()
+        {
+            return "Diese Seite sollte auch ohne Authentifizierung sichtbar sein :-)";
+        }
+    }
+}
