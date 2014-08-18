28448cbb66f12fc222ce8046a47e8d277929e458
Julius Heins <hz@iwb.tum.de>
2014-08-18 17:14:52 +0200

    Beschlüsse lassen sich nun filtern und separat durchsuchen

--- ILK-Protokoll/Controllers/DecisionsController.cs
+++ ILK-Protokoll/Controllers/DecisionsController.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using ILK_Protokoll.DataLayer;
 using ILK_Protokoll.Models;
+using ILK_Protokoll.ViewModels;
 
 namespace ILK_Protokoll.Controllers
 {
@@ -20,21 +17,88 @@ namespace ILK_Protokoll.Controllers
 		}
 
 		// GET: Decisions
-		public ActionResult Index()
+		public ActionResult Index(FilteredDecisions filter)
 		{
-			var decisions = db.Decisions
+			IQueryable<Decision> query = db.Decisions
 				.Include(d => d.OriginTopic)
 				.Include(d => d.Report);
 
-			return View(decisions.ToList());
+			if (!filter.ShowClosed)
+				query = query.Where(d => d.Type != DecisionType.Closed);
+
+			if (!filter.ShowResolution)
+				query = query.Where(d => d.Type != DecisionType.Resolution);
+
+
+			if (filter.Timespan != 0)
+			{
+				if (filter.Timespan == -1) // Zeitfenster berücksichtigen
+				{
+					var cutoff = filter.WindowEndDate.AddDays(1); // Inklusivgrenzen
+					query = query.Where(d => d.Report.End >= filter.WindowStartDate && d.Report.End <= cutoff);
+				}
+				else
+				{
+					var cutoff = DateTime.Today.AddDays(-filter.Timespan);
+					query = query.Where(d => d.Report.End > cutoff);
+				}
+			}
+
+			if (!string.IsNullOrWhiteSpace(filter.Searchterm))
+				query = query.Where(d => d.Text.Contains(filter.Searchterm) || d.OriginTopic.Title.Contains(filter.Searchterm));
+
+			filter.Decisions = query.OrderBy(d => d.Report.End).ToList();
+			filter.TimespanChoices = TimespanChoices(filter.Timespan);
+			return View(filter);
+		}
+
+		private static IEnumerable<SelectListItem> TimespanChoices(int preselect)
+		{
+			return new[]
+			{
+				new SelectListItem
+				{
+					Text = "(Zeitraum beliebig)",
+					Value = "0",
+					Selected = preselect == 0
+				},
+				new SelectListItem
+				{
+					Text = "1 Tag",
+					Value = "1",
+					Selected = preselect == 1
+				},
+				new SelectListItem
+				{
+					Text = "7 Tage",
+					Value = "7",
+					Selected = preselect == 7
+				},
+				new SelectListItem
+				{
+					Text = "30 Tage",
+					Value = "30",
+					Selected = preselect == 30
+				},
+				new SelectListItem
+				{
+					Text = "100 Tage",
+					Value = "100",
+					Selected = preselect == 100
+				},
+				new SelectListItem
+				{
+					Text = "Benutzerdefiniert",
+					Value = "-1",
+					Selected = preselect == -1
+				}
+			};
 		}
 
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing)
-			{
 				db.Dispose();
-			}
 			base.Dispose(disposing);
 		}
 	}
---
+++ ILK-Protokoll/ViewModels/FilteredDecisions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using ILK_Protokoll.Models;
+
+namespace ILK_Protokoll.ViewModels
+{
+	public class FilteredDecisions
+	{
+		public FilteredDecisions()
+		{
+			ShowClosed = false;
+			ShowResolution = true;
+			Searchterm = "";
+			Timespan = 0;
+			WindowStartDate = DateTime.Today.AddDays(-3);
+			WindowEndDate = DateTime.Today;
+
+// ReSharper disable once DoNotCallOverridableMethodsInConstructor
+			Decisions = new List<Decision>();
+		}
+
+		[DisplayName("Archiviert")]
+		public bool ShowClosed { get; set; }
+
+		[DisplayName("Beschluss")]
+		public bool ShowResolution { get; set; }
+
+		[DisplayName("Suchbegriff")]
+		public string Searchterm { get; set; }
+
+		/// <summary>
+		///    Zeitspanne in Tagen, die durchsucht werden soll. 0, wenn keine Einschränkung getroffen werden soll. -1 wenn das
+		///    Zeitfenster über die Properties WindowStartDate und WindowEndDate engegebn wird.
+		/// </summary>
+		public int Timespan { get; set; }
+
+		public IEnumerable<SelectListItem> TimespanChoices { get; set; }
+
+		[DisplayName("Von")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime WindowStartDate { get; set; }
+
+		[DisplayName("Bis")]
+		[DataType(DataType.Date)]
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public DateTime WindowEndDate { get; set; }
+
+		public virtual ICollection<Decision> Decisions { get; set; }
+	}
+}
