7a03418da5ea61c04907eb8170385cdce963e8e7
Julius Heins <hz@iwb.tum.de>
2014-08-18 10:58:40 +0200

    Kleine Hilfe um die Aufgabenplanung einzurichten

--- ILK-Protokoll/runTasksManually.ps1
+++ ILK-Protokoll/runTasksManually.ps1
@@ -1,1 +1,4 @@
 (New-Object Net.WebClient).DownloadString("http://02mucilk/ScheduledTasks")
+# Kommandozeile für die Aufgabenplanung:
+# Befehl: PowerShell
+# Parameter: -Command "& {(New-Object Net.WebClient).DownloadString(\"http://02mucilk/ScheduledTasks\")}"
