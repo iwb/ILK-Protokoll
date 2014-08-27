6b1f48fc89ca9f740f21dc9a4b51338c7c763e90
Julius Heins <hz@iwb.tum.de>
2014-08-27 11:20:01 +0200

    2f2f956e58691b771885a7082085866b4e85cc99 rückgängig

--- ILK-Protokoll/Global.asax.cs
+++ ILK-Protokoll/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -19,14 +18,6 @@ namespace ILK_Protokoll
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 
 			Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataContext, Configuration>());
-
-			using (var context = new DataContext())
-			{
-				if (!context.Database.Exists())
-				{
-					((IObjectContextAdapter)context).ObjectContext.CreateDatabase();
-				}
-			}
 
 #if DEBUG
 			foreach (Bundle bundle in BundleTable.Bundles)
